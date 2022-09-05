48c27f79e60635d9038c7a151a98291147804c4c
OK <@mail.ru>
2022-09-05 12:44:01 +0300

    add 002

---
+++ 001/Program.cs
@@ -0,0 +1,27 @@
+﻿/*/Задача 64: Задайте значения M и N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""/*/
+
+
+Console.WriteLine("Введите значение M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M={M}, N={N}");
+Console.WriteLine("Натуральные  числа в промежутке от M до N");
+Console.WriteLine($"{rec(M, N)}");
+int rec(int M, int N)
+{
+    if (M == N) return M;
+    else
+        Console.Write($"{rec(M, N - 1)}, ");
+    return N;
+}
+
+
+
+
+
+
+
