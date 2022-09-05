233ccfe85fa363d9527f9f11ad5b39e03b85e10f
OK <@mail.ru>
2022-09-05 17:32:42 +0300

    add 004

---
+++ 004/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 1. Семинар. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M={M}");
+Console.WriteLine("Сумма цифр числа");
+Console.WriteLine($"{rec(M)}");
+int rec(int M)
+{
+    if (M < 10) return M;
+    else
+        return M % 10 + rec(M / 10);
+}
+
